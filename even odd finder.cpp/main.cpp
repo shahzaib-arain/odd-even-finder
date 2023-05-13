d293dd96478ea81eee5ac6fdff0900a2d69119d0
shahzaib-arain <133527622+shahzaib-arain@users.noreply.github.com>
2023-05-13 11:33:41 -0700

    Function with params

--- even odd finder.cpp/main.cpp
+++ even odd finder.cpp/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
 using namespace std;
-void even_odd_finder()
+void even_odd_finder(int number)
 {
-        int number;
-    cout<<"enter the number :";
-    cin>>number;
+
     if(number%2==0)
     {
    cout<<" the number "<<number<< " is even ";
@@ -18,7 +16,7 @@ void even_odd_finder()
 
  main()
 {
-even_odd_finder();
+even_odd_finder(65);
 
 
     return 0;
